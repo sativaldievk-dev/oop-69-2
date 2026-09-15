fce2be6caabf22e5b25164ebd026a3d131bc12f6
sativaldievk@gmail.com <sativaldievk@gmail.com>
2026-09-15 14:09:05 +0600

    Удалил старые домашние задания

--- handlers/echo.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Router, F
-from aiogram.types import Message
-router=Router()
-
-@router.message(F.text)
-async def echo(message:Message):
-    await message.answer("До встречи!")
